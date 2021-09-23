0f4bf720782c54b0f201eea11ad4a56cc50d6b3c
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-09-23 22:18:44 +0200

    input with debounce done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+import { debounce } from 'lodash';
+
+
+//const debounce = require('lodash.debounce');
+
+const inputCountry = document.getElementById('input-country')
+inputCountry.addEventListener('input', debounce(callback, 500))
+
+
+function callback() {
+    console.log(inputCountry);
+}
