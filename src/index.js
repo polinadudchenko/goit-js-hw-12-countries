a3797d4cd19e677054dd3b649f8b4a455d925ceb
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-09-24 23:35:49 +0200

    handlebars markup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import { debounce } from 'lodash';
+import countryCardTpl from './templates/country-card.hbs'
 
 
 //const debounce = require('lodash.debounce');
@@ -7,7 +8,17 @@ import { debounce } from 'lodash';
 const inputCountry = document.getElementById('input-country')
 inputCountry.addEventListener('input', debounce(callback, 500))
 
-
+//const searchQuery = inputCountry    ${searchQuery}
 function callback() {
     console.log(inputCountry);
 }
+
+fetch(`https://restcountries.com/v3/name/belgien`)
+    .then(response => {
+        return response.json();
+    }).then(country => {
+        const markup = countryCardTpl(country);
+        console.log(markup);
+    }).catch(err => {
+    console.log(err);
+})
