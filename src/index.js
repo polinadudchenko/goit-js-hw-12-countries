69b6fe442a31e21c69ac06ab844e0411c973923b
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-09-25 00:34:07 +0200

    render html on the site

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import './sass/main.scss';
 import { debounce } from 'lodash';
 import countryCardTpl from './templates/country-card.hbs'
 
-
-//const debounce = require('lodash.debounce');
-
-const inputCountry = document.getElementById('input-country')
-inputCountry.addEventListener('input', debounce(callback, 500))
-
-//const searchQuery = inputCountry    ${searchQuery}
-function callback() {
-    console.log(inputCountry);
+const refs = {
+    cardContainer: document.querySelector('.js-card-container'),
+    inputCountry: document.getElementById('input-country'),
 }
 
-fetch(`https://restcountries.com/v3/name/belgien`)
-    .then(response => {
-        return response.json();
-    }).then(country => {
-        const markup = countryCardTpl(country);
-        console.log(markup);
-    }).catch(err => {
-    console.log(err);
-})
+const { cardContainer, inputCountry } = refs;
+
+//const inputCountry = document.getElementById('input-country')
+inputCountry.addEventListener('input', debounce(callback, 500))
+
+
+function callback(e) {
+    e.preventDefault();
+    const searchQuery = e.target.value;
+    fetch(`https://restcountries.com/v3/name/${searchQuery}`)
+        .then(response => {
+            return response.json();
+        }).then(country => {
+            const markup = countryCardTpl(country);
+            cardContainer.innerHTML = markup;
+        }).catch(err => {
+            console.log(err);
+        })
+}
