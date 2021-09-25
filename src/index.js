fbe340c1237c21f1d764b1191d659ab7224dd67b
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-09-25 12:59:13 +0200

    conditions for fetch done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './sass/main.scss';
 import { debounce } from 'lodash';
 import countryCardTpl from './templates/country-card.hbs'
+import countryListTpl from './templates/country-list.hbs'
 import API from './js/fetchCountries'
 import getRefs from './js/get-refs'
+import {error, alert, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+defaultModules.set(PNotifyMobile, {});
 
 const { cardContainer, inputCountry } = getRefs();
 inputCountry.addEventListener('input', debounce(callback, 500))
@@ -10,17 +16,34 @@ inputCountry.addEventListener('input', debounce(callback, 500))
 
 function callback(e) {
     e.preventDefault();
-    const searchQuery = e.target.value;
-    API.fetchCountry(searchQuery).then(renderCountryCard)
-        .catch(onFetchError)
-        //.finally(reset())
+    const searchQuery = e.target.value.trim();
+    API.fetchCountry(searchQuery).then(renderCountryCard).catch(onFetchError).finally(clearInput)
 }
 
 function renderCountryCard(country) {
-    const markup = countryCardTpl(country);
-    cardContainer.innerHTML = markup;
+    let markup;
+    console.log(country);
+    if (country.length == 1) {
+        markup = countryCardTpl(country);
+        cardContainer.innerHTML = markup;
+    } else if (country.length > 1 && country.length < 11) {
+        markup = countryListTpl(country);
+        cardContainer.innerHTML = markup;
+    } else if (country.length > 10) {
+        alert({ text: 'Too many matches. Please precise your request' })
+    }  else {
+        throw (country)
+    }
 }
 
-function onFetchError() {
-    alert('aaaaaaaa')
+function  onFetchError(err) {
+    if (err.status === 404) {
+        error({
+            text: 'No matches found, please enter a new query.'
+        })
+    }
+}
+
+function clearInput() {
+    inputCountry.value = ''
 }
